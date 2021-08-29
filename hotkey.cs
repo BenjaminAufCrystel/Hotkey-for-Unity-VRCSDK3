f3fe1657a73c81ab04fff7dca8562b3254f1a274
BenjaminAufCrystel <80203784+BenjaminAufCrystel@users.noreply.github.com>
2021-08-29 22:18:40 +0200

    UdonSharp

---
+++ hotkey.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using System.Collections;
+
+public class hotkey : UdonSharpBehaviour
+{
+
+    public GameObject Target; 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Target.SetActive(!Target.activeSelf);
+        }
+    }
+}
